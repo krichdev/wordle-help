5dcdf12234034ce45e861f382e7c506b9bd77e7b
Kyle Richardson <kyle@alkymi.io>
2022-02-04 23:11:33 -0800

    adding starts with routes

--- pages/api/words.ts
+++ pages/api/words.ts
@@ -1,29 +1,41 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { globalWordSearch } from '../../lib/wordSearch';
+import { globalWordSearch, wordsThatStartWith } from '../../lib/wordSearch';
 
-type Data = {
+export type WordsData = {
   words: string[] | never[],
   message: string,
 }
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<WordsData>
 ) {
-  const { body } = req;
+  if (req.method === 'POST') {
+    const { body } = req;
 
-  const words = globalWordSearch(body);
+    const words = globalWordSearch(body);
 
-  if (words.length <= 0) {
-    return res.status(200).json({
+    if (words.length <= 0) {
+      return res.status(200).json({
+        words,
+        message: 'No possible words found.'
+      })
+    }
+
+    res.status(200).json({
       words,
-      message: 'No possible words found.'
-    })
+      message: `There are ${words.length} possible words`,
+    });
   }
 
-  res.status(200).json({
-    words,
-    message: `There are ${words.length} possible words`,
-  });
+  if (req.method === 'GET') {
+    const { query } = req;
+
+    const words = wordsThatStartWith(query.starts as string);
+    return res.status(200).json({
+      words,
+      message: `There are ${words.length} possible words`,
+    })
+  }
 }
---
+++ pages/words-that-start-with/z.tsx
@@ -0,0 +1,55 @@
+import Head from 'next/head'
+import Link from 'next/link';
+
+interface Props {
+  data: {
+    words: string[],
+    message: string,
+  }
+}
+
+const StartsWithZ = ({ data }: Props) => {
+   return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Wordle Words That Start with Z</title>
+        <link rel="icon" href="/wordlefav.ico" />
+        <meta property='og:image' content='/wordleimg.png' key='ogimage' />
+      </Head>
+
+      <main className="flex w-full flex-1 flex-col items-center mt-20 px-20 text-center">
+        <h1 className="text-6xl font-bold mb-8">
+          {data.words.length} Wordle Words That Start With Z
+        </h1>
+        <Link href="/">
+          <p className='w-[350px] h-12 px-6 mt-8 text-indigo-100 flex items-center justify-center cursor-pointer transition-colors duration-150 bg-blue-600 rounded-lg'>Enter Letters</p>
+        </Link>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:gird-cols-3 gap-4'>
+          {data.words.map(word => (
+            <div key={word} className='grid grid-cols-5 gap-2 w-[350px] h-[62px] mt-8'>
+              {word.split('').map((letter, i) => (
+                <div key={`${letter}-${word}-${i}`} className={`w-full ${i <= 0 ? 'bg-[#6aaa64] text-white border-none' : 'bg-white'} border-slate-300 border-2 flex items-center justify-center font-extrabold text-3xl`}>
+                  {letter.toUpperCase()}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default StartsWithZ
+
+export async function getStaticProps() {
+  const path = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://wordlehelps.com';
+  const res = await fetch(`${path}/api/words?starts=z`);
+  const data = await res.json();
+
+  return {
+    props: {
+      data
+    }
+  }
+}
